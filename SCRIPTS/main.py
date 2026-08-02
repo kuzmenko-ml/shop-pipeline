2ff13fe04e236612a87de2df5afa7884e5145cd1
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-08-02 10:53:34 +0300

    feat(main): use second pipeline for saving to parquet

--- SCRIPTS/main.py
+++ SCRIPTS/main.py
@@ -1,4 +1,5 @@
 from ingestion_pipeline import IngestionPipeline
+from load_pipeline import LoadPipeline
 
 def main_procces():
     pipeline_ingestion = IngestionPipeline()
@@ -19,6 +20,14 @@ def main_procces():
     sql_query_items = "SELECT * FROM order_items"
     data_db_items = pipeline_ingestion.read_db_data(sql_query_items)
     print(data_db_items)
+
+    loader = LoadPipeline()
+
+    loader.save_to_parquet(data_products, "products.parquet")
+    loader.save_to_parquet(data_users, "users.parquet")
+    loader.save_to_parquet(data_db_statuses, "db_statuses.parquet")
+    loader.save_to_parquet(data_db_orders, "db_orders.parquet")
+    loader.save_to_parquet(data_db_items, "db_items.parquet")
 
 if __name__ == "__main__":
     main_procces()
